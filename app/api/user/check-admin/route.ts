46bf0b9827fc134d2d77a12da04fecff2369d667
FrostBreker <frostbreker@gmail.com>
2026-01-31 11:47:52 +0100

    refactor: update admin check in route to use session-based authentication
    Signed-off-by: FrostBreker <frostbreker@gmail.com>

--- app/api/user/check-admin/route.ts
+++ app/api/user/check-admin/route.ts
@@ -1,12 +1,18 @@
 import { NextResponse } from "next/server";
-import {requestAdminCheck} from "@lib/api/request_check";
+import {getServerSession} from "next-auth";
+import {authOptions} from "@api/auth/[...nextauth]/route";
+import {isAdmin} from "@lib/user/checkAdmin";
 
 export async function GET() {
     try {
-        const isAuthorized = await requestAdminCheck();
-        if (!isAuthorized) return;
+        const session = await getServerSession(authOptions);
+        if (!session || !session.user?.email) {
+            return NextResponse.json({ isAdmin: false });
+        }
 
-        return NextResponse.json({ isAdmin: isAuthorized });
+        const userIsAdmin = await isAdmin();
+
+        return NextResponse.json({ isAdmin: userIsAdmin });
     } catch (error) {
         console.error("Error checking admin:", error);
         return NextResponse.json({ isAdmin: false });
